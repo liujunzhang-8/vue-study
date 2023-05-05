1dc263be3bc5506efc32afb22d1305aaf629a099
Gorgio Liu <junchang.liu@meditrusthealth.cn>
2023-05-05 18:23:16 +0800

    watch

--- demo-01/src/App.vue
+++ demo-01/src/App.vue
@@ -1,54 +1,80 @@
 <template>
   <div id="app">
-    姓：<input type="text" v-model='firstName'><br /><br />
-    名：<input type="text" v-model='lastName'><br /><br />
-    姓名：<span>{{ fullName }}</span>
+    <h1>今天天气很{{ info}}</h1>
+    <button @click="changeWeather">切换天气</button>
+    <hr />
+    <h3>a的值时：{{ numbers.a }}</h3>
+    <button @click="numbers.a++">点击让a+1</button>
+    <h3>b的值时：{{ numbers.b }}</h3>
+    <button @click="numbers.b++">点击让a+1</button>
   </div>
 </template>
 
 <script>
 /**
- * 计算属性：
- *  1. 定义：要用的属性不存在，要通过已有的属性计算得来。
- *  2. 原理：底层借助了Object.defineproperty方法提供的getter和setter。
- *  3. get函数什么时候执行？
- *    (1). 初次读取时会执行一次。
- *    (2). 当依赖的数据发生改变时会被再次调用。
- *  4. 优势：与methods实现相比，内部有缓存机制(复用)，效率更高，调试方便。
- *  5. 备注：
- *    1. 计算属性最终会出现在vm上，直接读取使用即可。
- *    2. 如果计算属性要被修改，那必须写set函数去响应修改，且set中要引起计算时依赖的数据发生改变。
+ * 监视属性watch：
+ *  1. 当被监视的属性变化时，回调函数自动调用，进行相关操作
+ *  2. 监视的属性必须存在，才能进行监视！！
+ *  3. 监视的两种写法：
+ *    (1). new Vue时传入watch配置
+ *    (2). 通过vm.$watch监视
+ * 
+ * 深度监视：
+ *  1. Vue中的watch默认不监测对象内部值的改变（一层）
+ *  2. 配置 deep: true 可以监测对象内部值改变（多层）
+ *  备注：
+ *    (1). Vue自身可以监测对象内部值的改变，但Vue提供的watch默认不可以！
+ *    (2). 使用watch时根据数据的具体结构，决定是否采用深度监视。
  */
 export default {
   name: 'App',
   data() {
     return {
-      firstName: 'zhang',
-      lastName: 'san'
+      isHot: true,
+      numbers: {
+        a: 1,
+        b: 2
+      }
     }
   },
   computed: {
+    info() {
+      return this.isHot ? '炎热' : '凉爽'
+    }
+  },
+  watch: {
     // 完整写法
-    // fullName: {
-    //   // get有什么作用？当有人读取fullName时，get就会被调用，且返回值就作为fullName的值
-    //   // get什么时候调用？1. 初次读取fullName时；2. 所依赖的数据发生变化时
-    //   get() {
-    //     console.log('111');
-    //     return this.firstName + '-' + this.lastName
-    //   },
-    //   // set什么时候调用？当fullName被修改时
-    //   set(value) {
-    //     console.log(value);
-    //     const arr = value.split('-')
-    //     this.firstName = arr[0]
-    //     this.lastName = arr[1]
+    // isHot: {
+    //   // immediate: true,  // 初始化时让handler调用一下
+    //   // deep: true, 深度监视
+    //   // handler什么时候调用？当isHot发生改变时
+    //   handler(newvalue, oldValue) {
+    //     console.log(newvalue, oldValue, 'isHot被修改了');
+    //   }
+    // },
+    // 简写： 
+    isHot(newvalue, oldValue) {
+      console.log(newvalue, oldValue, 'isHot被修改了');
+    }
+    // 监视多级结构中某个属性的变化
+    // 'numbers.a': {
+    //   handler() {
+    //     console.log('a被改变了');
     //   }
     // }
-    // 简写
-    fullName() {
-      return this.firstName + '-' + this.lastName
+    // 监视多级结构中所有属性的变化
+    // numbers: {
+    //   deep: true,
+    //   handler() {
+    //     console.log('number改变了');
+    //   }
+    // }
+  },
+  methods: {
+    changeWeather() {
+      this.isHot = !this.isHot
     }
-  }
+  },
 
 }
 </script>
