2532373dabf987824286b965d4ba7cd8b3feddc3
Gorgio Liu <junchang.liu@meditrusthealth.cn>
2023-05-08 09:43:19 +0800

    绑定样式

--- demo-01/src/App.vue
+++ demo-01/src/App.vue
@@ -1,28 +1,38 @@
 <template>
   <div id="app">
-    姓：<input type="text" v-model='firstName'><br /><br />
-    名：<input type="text" v-model='lastName'><br /><br />
-    姓名：<span>{{ fullName }}</span>
+    <!-- 绑定class样式 -- 字符串写法，适用于：样式的类名不确定，需要动态指定 -->
+    <div class="basic" :class="mood">{{ name }}</div>
+
+    <!-- 绑定class样式 -- 数组写法，适用于：要绑定的样式个数不确定、名字也不确定 -->
+    <div class="basic" :class="classArr">{{ name }}</div>
+
+    <!-- 绑定class样式 -- 对象写法，适用于：要绑定的样式个数确定、名字也确定，但要动态决定用不用 -->
+    <div class="basic" :class="classArr">{{ name }}</div>
   </div>
 </template>
 
 <script>
 /**
- * computed 和 watch 之间的区别：
- *  1. computed 能完成的功能，watch 都可以完成。
- *  2. watch 能完成的功能，computed 不一定能完成，例如：watch可以进行异步操作。
- * 两个重要的小原则：
- *  1. 所被Vue管理的函数，最好写成普通函数，这样this的指向才是vm 或 组件实例对象。
- *  2. 所有不被Vue管理的函数（定时器的回调函数、ajax的回调函数等、Promise的回调函数），
- *     最好写成箭头函数，这样this的指向才是vm 或 组件实例对象
+ * 绑定样式：
+ *  1. class样式
+ *    写法：class=“xxx”，xxx可以是字符串，对象，数组。
+ *    对象写法适用于：要绑定多个样式，个数不确定，名字也不确定。
+ *    数组写法适用于：要绑定多个样式，个数确定，名字也确定，但不确定用不用。
+ *  2. style样式
+ *    :style="{fontSize: xxx}" 其中xxx是动态值
+ *    :style="[a, b]" 其中a、b是样式对象
  */
 export default {
   name: 'App',
   data() {
     return {
-      firstName: 'zhang',
-      lastName: 'san',
-      fullName: 'zhang-san'
+      name: 'zhang-san',
+      mood: 'normal',
+      classArr: ['class1', 'class2', 'class3'],
+      classObj: {
+        class1: false,
+        class2: false
+      }
     }
   },
   computed: {
