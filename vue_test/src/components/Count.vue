e0366a0351b0ae67afa309fc6598a48f0a060fa1
Gorgio Liu <junchang.liu@meditrusthealth.cn>
2023-05-17 16:21:09 +0800

    vue版求和案例

---
+++ vue_test/src/components/Count.vue
@@ -0,0 +1,58 @@
+<!--
+ * @Descripttion: 
+ * @Author: Gorgio.Liu
+ * @version: 
+ * @Date: 2023-05-16 17:14:48
+ * @LastEditors: Gorgio.Liu
+ * @LastEditTime: 2023-05-17 16:18:28
+-->
+<template>
+  <div class="count">
+    <h2>当前求和为：{{ sum }}</h2>
+    <select v-model.number="n">
+      <option :value="1">1</option>
+      <option :value="2">2</option>
+      <option :value="3">3</option>
+    </select>
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+    <button @click="incrementOdd">当前求和为奇数再加</button>
+    <button @click="incrementWait">等一等再加</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VCount",
+  data() {
+    return {
+      n: 1, // 用户选择的数据
+      sum: 0, // 当前的和
+    };
+  },
+  methods: {
+    increment() {
+      this.sum += this.n;
+    },
+    decrement() {
+      this.sum -= this.n;
+    },
+    incrementOdd() {
+      if(this.sun % 2) {
+        this.sum += this.n;
+      }
+    },
+    incrementWait() {
+      setTimeout(() => {
+        this.sum += this.n;
+      }, 500)
+    },
+  },
+};
+</script>
+
+<style>
+button {
+  margin-left: 10px;
+}
+</style>
