fad8b313b0187dc0b957cf43c3e2a2f906893670
Gorgio Liu <junchang.liu@meditrusthealth.cn>
2023-05-17 17:20:27 +0800

    vuex

--- vue_test/src/main.js
+++ vue_test/src/main.js
@@ -3,8 +3,12 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// 引入store
+import store from './store'
+
 new Vue({
   render: h => h(App),
+  store,
   beforeCreate() {
     Vue.prototype.$bus = this // 安装全局事件总线
   }
