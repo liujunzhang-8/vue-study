12015f232f21cdf81a1e991929bfc5858cd71f5b
Gorgio Liu <junchang.liu@meditrusthealth.cn>
2023-05-21 21:36:18 +0800

    watch

---
+++ vue3_test/05_src_watch函数/App.vue
@@ -0,0 +1,34 @@
+<!--
+ * @Descripttion: 
+ * @Author: Gorgio.Liu
+ * @version: 
+ * @Date: 2023-05-19 16:23:44
+ * @LastEditors: Gorgio.Liu
+ * @LastEditTime: 2023-05-19 20:07:33
+-->
+<template>
+  <div>
+    <VDemo msg="你好" />
+  </div>
+</template>
+
+<script>
+import VDemo from './components/Demo'
+export default {
+  name: 'App',
+  components: {VDemo},
+  setup() {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
