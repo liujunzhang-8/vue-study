61e4874239503e6c5f905c90b310d9659b3ed132
Gorgio Liu <junchang.liu@meditrusthealth.cn>
2023-05-22 10:34:18 +0800

    toRaw与markRaw

---
+++ vue3_test/08_src_toRaw与markRaw/App.vue
@@ -0,0 +1,41 @@
+<!--
+ * @Descripttion: 
+ * @Author: Gorgio.Liu
+ * @version: 
+ * @Date: 2023-05-19 16:23:44
+ * @LastEditors: Gorgio.Liu
+ * @LastEditTime: 2023-05-21 22:07:42
+-->
+<template>
+  <div>
+    <VDemo v-if="isShowDemo" />
+    <button @click="isShowDemo = !isShowDemo">切换隐藏/显示</button>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+import VDemo from './components/Demo'
+export default {
+  name: 'App',
+  components: {VDemo},
+  setup() {
+    let isShowDemo = ref(true)
+
+    return {
+      isShowDemo
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
