a7693ba57477bb51fc744b540d85eeb0f4ddf124
Gorgio Liu <junchang.liu@meditrusthealth.cn>
2023-05-22 11:25:41 +0800

    祖孙间通信

---
+++ vue3_test/src/components/Child.vue
@@ -0,0 +1,29 @@
+<!--
+ * @Descripttion: 
+ * @Author: Gorgio.Liu
+ * @version: 
+ * @Date: 2023-05-22 11:11:07
+ * @LastEditors: Gorgio.Liu
+ * @LastEditTime: 2023-05-22 11:15:05
+-->
+<template>
+  <div class='child'>
+    <h2>这是Child组件(子组件)</h2>
+    <VSon />
+  </div>
+</template>
+
+<script>
+import VSon from './Son'
+export default {
+  name: 'VChild',
+  components: {VSon}
+}
+
+</script>
+<style scoped>
+.child {
+  background-color: skyblue;
+  padding: 10px;
+}
+</style>
---
+++ vue3_test/src/components/Son.vue
@@ -0,0 +1,34 @@
+<!--
+ * @Descripttion: 
+ * @Author: Gorgio.Liu
+ * @version: 
+ * @Date: 2023-05-22 11:11:14
+ * @LastEditors: Gorgio.Liu
+ * @LastEditTime: 2023-05-22 11:21:12
+-->
+<template>
+  <div class='son'>
+    <h2>这是Son组件(孙组件), {{ car.name }} --- {{ car.price }}</h2>
+  </div>
+</template>
+
+<script>
+import { inject } from 'vue';
+export default {
+  name: 'VSon',
+  setup() {
+    let car = inject('car')
+
+    return {
+      car
+    }
+  }
+}
+
+</script>
+<style scoped>
+.son {
+  background-color: orange;
+  padding: 10px;
+}
+</style>
